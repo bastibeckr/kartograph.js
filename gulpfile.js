0b632e823e054596b50c2282cc72f6f6258914fd
Basti Becker <bastibeckr@gmail.com>
2016-06-29 10:59:29 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var del = require('del');
+var manifest = require('asset-builder')('manifest.json');
+
+var kartographDeps = manifest.getDependencyByName('kartograph.js');
+
+
+gulp.task('clean', function () {
+  return del([
+    manifest.paths.dist
+  ]);
+});
+
+gulp.task('coffee', ['clean'], function() {
+
+    gutil.log(JSON.stringify(manifest, null, 2));
+
+    gulp.src(kartographDeps.globs)
+        .pipe(coffee({
+            bare: true
+        }).on('error', gutil.log))
+        .pipe(concat('kartograph.js'))
+        .pipe(gulp.dest(manifest.paths.dist));
+
+});
+
+
